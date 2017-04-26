4d79b37d8d2c1ec63caa29aa49025fce394f2875
bsantanas <bsantana@hellobundle.com>
2017-04-25 22:52:14 -0400

    Initial Commit

---
+++ ThinkriteTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThinkriteTest
+//
+//  Created by Bernardo Santana on 4/25/17.
+//  Copyright © 2017 bsantanas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThinkriteTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThinkriteTest
+//
+//  Created by Bernardo Santana on 4/25/17.
+//  Copyright © 2017 bsantanas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
